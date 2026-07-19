fb5a3517c22a0d1c2a47c32a4e61b21f98ac57d0
林毅玮 <18601013891@163.com>
2026-07-19 22:28:31 +0800

    This is the cs50 part for SQL for the database code. ---2026/7/19

--- cs50/favorites.py
+++ cs50/favorites.py
@@ -1,14 +1,25 @@
-import csv
+# import csv
+
+# # with open("favorites.csv", "r") as file:
+# #     reader = csv.DictReader(file)
+# #     for row in reader:
+# #         # favorite = row["player_name"]
+# #         print(row["player_name"])
 
 # with open("favorites.csv", "r") as file:
 #     reader = csv.DictReader(file)
+#     # scratch, c, python = 0, 0, 0
+#     Spanish, South_Africa, Dutch = 0, 0, 0
+#     # for row in reader:
+#     #     favorite = row["height_cm"]
+#     #     print(favorite)
 #     for row in reader:
-#         # favorite = row["player_name"]
-#         print(row["player_name"])
-
-with open("favorites.csv", "r") as file:
-    reader = csv.DicReader(file)
-    scratch, c, python = 0, 0, 0
-    for row in reader:
-        favorite = row["language"]
-        
+#         favorite = row["nationality"]
+#         if favorite == "Spanish":
+#             Spanish += 1
+#         elif favorite == "South African":
+#             South_Africa += 1
+#         elif favorite == "Dutch":
+#             Dutch += 1
+    
+#     print((Spanish, South_Africa, Dutch))
