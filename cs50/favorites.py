48f2d31fae0555b043a026509b3bf9592e6d2da8
林毅玮 <18601013891@163.com>
2026-07-20 12:27:48 +0800

    This is the end of the SQL for the cs50 class ---2026/7/20

--- cs50/favorites.py
+++ cs50/favorites.py
@@ -23,3 +23,14 @@
 #             Dutch += 1
     
 #     print((Spanish, South_Africa, Dutch))
+
+# from cs50 import SQL
+
+# db = SQL("sqlite:///favorites.db")
+
+# favorite = input("Favorite: ")
+
+# rows = db.execute("select count(*) as n from favorites where nationality = ?")
+# row = rows[0]
+
+# print(row["n"])
